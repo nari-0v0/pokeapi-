2ffe849492c4b2aad0fbe04d421b5f1d50de9e0d
nari-0v0 <ej9811@naver.com>
2025-07-21 17:22:27 +0900

    오류수정

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux';
 import { Card } from '../component/Card';
 
 export default function Main() {
-  const pokemonData = useSelector((state) => state.pokemon.data);
+  const pokemonData = useSelector((state) => state.pokemon);
 
   return (
     <>
